283b9823adcfc980de35f0146480801f6edbcd52
Yuying-li-1 <yul006@ucsd.edu>
2021-02-12 23:10:17 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -18,6 +18,16 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 //  console.log(localStorage.length);
+var list = [];
+
+function checkList() {
+  return localStorage.getItem('list') == null ? false : true;
+};
+
+if(checkList()){
+  list = localStorage.getItem('list').split(',');
+  document.getElementById('cart-count').textContent = (list.length).toString();
+}
 
   var product_list = document.getElementById("product-list");
 
@@ -29,21 +39,45 @@ window.addEventListener('DOMContentLoaded', () => {
     //console.log(temp.title);
     temp.price = fromJS['price'];
     temp.src = fromJS['image'];
-
+    if(list.indexOf(temp.id) < 0){
+      temp.add = 0;
+    }
+    else{
+      temp.add = 1;
+    }
     product_list.appendChild(temp);
+    addToCart(temp,list);
+
+
 
   }
 
   });
 
-function addToCart(){
+function addToCart(shad, list){
+  const shadow = shad.shadowRoot;
+  var tempBut = shadow.querySelector('button');
+  tempBut.addEventListener('click', function(){
+  var counts = parseInt(document.getElementById('cart-count').textContent);
+  if(shad.add == 0){
+    shad.add = 1;
+    counts += 1;
+    list.push(shad.id);
+    document.getElementById('cart-count').textContent = counts.toString();
+    localStorage.setItem('list', list);
+  }
 
+  else if(shad.add == 1){
+    shad.add = 0;
+    counts -= 1;
+    list.splice(list.indexOf(shad.id), 1);
+    document.getElementById('cart-count').textContent = counts.toString();
+  }
+
+  });
 }
 
 //localStorage.clear();
-  /*  if(checkStorage()){
-    list = (localStorage.getItem('list')).split(',');
-  }*/
 
 
 
