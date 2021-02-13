c642de2d5cdc18452a48d55447645a7afbb377e0
Yuying-li-1 <yul006@ucsd.edu>
2021-02-12 17:01:05 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,19 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+  function checkStorage() {
+    return JSON.stringify(localStorage) == "{}" ? false : true;
+  };
+  /*if(checkStorage){
+
+  }*/
+  fetch(`https://fakestoreapi.com/products`)
+    .then(res => res.json())
+    .then(json => {
+      contentString = JSON.stringify(json);
+      //console.log(contentString);
+      localStorage.setItem("items",contentString);
+    } );
+  //const content = response.json();
+
 });
